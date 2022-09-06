2a398aa93d927f6696e43da1ceb7388914d6e60c
voskanvl <voskanvl@gmail.com>
2022-09-06 12:29:32 +0300

    preinit 3

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import { MSplides } from "./initSlides"
 import '@splidejs/splide/css'
 
 const splidesInstance = new MSplides()
-splidesInstance.add('#test', { type: 'loop', autoplay: 1000 })
-console.log(splidesInstance.instances['#test'])
+splidesInstance.add('#test', { type: 'loop', autoplay: true })
+splidesInstance.add('#test1', { type: 'loop', autoplay: true, perPage: 2, perMove: 1, direction: 'rtl', arrows: false })
+
+const chairtypesButtons = document.querySelectorAll(".chairtypes__button")
+chairtypesButtons[0].addEventListener("click", () => {
+    splidesInstance.instances['#test1'].go('<')
+})
+chairtypesButtons[1].addEventListener("click", () => {
+    splidesInstance.instances['#test1'].go('>')
+})
