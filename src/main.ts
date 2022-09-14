938ee7f499cc724ef031a864e9db381d9fe2fd4f
voskanvl <voskanvl@gmail.com>
2022-09-14 14:24:30 +0300

    blog

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ splidesInstance.add('#chairtypes', {
     pagination: false,
     autoWidth: true 
 })
+splidesInstance.add('#blog-slider', { type: 'loop', arrows: false })
 
 const chairtypesButtons = document.querySelectorAll(".chairtypes__button")
 chairtypesButtons[0].addEventListener("click", () => {
@@ -21,6 +22,14 @@ chairtypesButtons[0].addEventListener("click", () => {
 })
 chairtypesButtons[1].addEventListener("click", () => {
     splidesInstance.instances['#chairtypes'].go('>')
+})
+
+const blogButtons = document.querySelectorAll(".blog__button")
+blogButtons[0].addEventListener("click", () => {
+    splidesInstance.instances['#blog-slider'].go('>')
+})
+blogButtons[1].addEventListener("click", () => {
+    splidesInstance.instances['#blog-slider'].go('<')
 })
 
 const audio: HTMLAudioElement | null = document.querySelector("audio")
