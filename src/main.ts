a742304f5c3be97880901ec75f28480f6c11e96e
voskanvl <voskanvl@gmail.com>
2022-11-15 19:55:51 +0300

    switch active role

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import "@splidejs/splide/css";
 import toggleSidePanel from "./toggleSlidePanel";
 import slides from "./slides";
 import selector from "./selector";
+import { tabs, storeTabs } from "./tabs";
 
 slides();
 
 toggleSidePanel(".side-panel", "#trigger");
 
 selector();
+
+tabs();
+storeTabs.subscribe((current: string) => {
+    const allListItems = Array.from(document.querySelectorAll<HTMLElement>(".list__item"));
+    allListItems.forEach(element => {
+        const dataIndex = element.dataset.index;
+        if (dataIndex === current) element.setAttribute("active", "active");
+        else element.removeAttribute("active");
+    });
+});
---
+++ src/tabs.ts
@@ -0,0 +1,50 @@
+export interface IStore {
+    currentTab: HTMLElement | null;
+    currentRole?: string;
+    tabs: HTMLElement[] | null;
+}
+
+export class StoreTabs implements IStore {
+    currentTab: HTMLElement | null = null;
+    private _currentRole: string = "";
+    tabs: HTMLElement[] | null = null;
+    subscribers: Function[] = [];
+
+    get currentRole() {
+        return this._currentRole;
+    }
+    set currentRole(x: string) {
+        this._currentRole = x;
+        this.subscribers.forEach(e => e(this._currentRole));
+    }
+    subscribe(cb: (data: string) => void) {
+        this.subscribers.push(cb);
+    }
+    unsubscribe(cb: (data: string) => void) {
+        this.subscribers = this.subscribers.filter(e => e !== cb);
+    }
+}
+
+export const storeTabs = new StoreTabs();
+export function tabs() {
+    const tabsContainerElement = document.querySelector<HTMLElement>(".tabs");
+    if (!tabsContainerElement) return;
+    const tabs = Array.from(tabsContainerElement.children).map(e => e as HTMLElement);
+    storeTabs.tabs = tabs;
+    detectActiveTab(storeTabs);
+    tabsContainerElement &&
+        tabsContainerElement.addEventListener("click", (e: Event) => {
+            const target = (e.target as HTMLElement).closest<HTMLButtonElement>(".tab");
+            storeTabs.currentTab && storeTabs.currentTab?.removeAttribute("active");
+            storeTabs.currentTab = target;
+            storeTabs.currentRole = target?.dataset.index;
+            console.log(storeTabs);
+        });
+}
+function detectActiveTab(store: IStore): void {
+    const active = store.tabs?.filter(e => e.getAttribute("active"))[0];
+    if (!active) return;
+    store.currentRole = active?.dataset.index;
+    store.currentTab = active;
+    console.log(store);
+}
