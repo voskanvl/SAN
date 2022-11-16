38564eb3690677d8f0bda2f167182445e2ce1e83
voskanvl <voskanvl@gmail.com>
2022-11-16 14:45:50 +0300

    booking-order

---
+++ src/booking-order.ts
@@ -0,0 +1,39 @@
+import IMask from "imask";
+
+export default function bookingOrder() {
+    const formElement = document.querySelector<HTMLFormElement>("form.style__form");
+    if (!formElement) return;
+    const inputNames = ["name", "phone", "email"];
+    type inputsType = {
+        name?: HTMLInputElement;
+        phone?: HTMLInputElement;
+        email?: HTMLInputElement;
+    };
+    const inputElements: inputsType = inputNames.reduce(
+        (acc, element) =>
+            Object.assign(acc, {
+                [element]: formElement.querySelector<HTMLInputElement>(`input[name="${element}"]`),
+            }),
+        {},
+    );
+    console.log("🚀 ~ inputElements", inputElements);
+    inputElements.phone &&
+        IMask(inputElements.phone, {
+            mask: "+{7}(000)000-00-00",
+            lazy: false,
+        });
+    inputElements.email &&
+        IMask(inputElements.email, {
+            mask(value) {
+                if (/^[a-z0-9_\.-]+$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@[a-z0-9-]+$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.$/.test(value)) return true;
+                if (/^[a-z0-9_\.-]+@[a-z0-9-]+\.[a-z]{1,4}\.[a-z]{1,4}$/.test(value)) return true;
+                return false;
+            },
+            lazy: false,
+        });
+}
